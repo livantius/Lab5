7d84720909069811c1771e12907d308788ccc91f
livantius <76242373+livantius@users.noreply.github.com>
2020-12-22 16:16:45 +0700

    Настройка интерфейса и функционала «MotionEvent» (два текстовых поля, кнопка невидима) и при прикосновении к экрану в тестовых окнах отображались координаты касаний и тип события

--- app/src/main/java/a/event_handling/MotionEvent.java
+++ app/src/main/java/a/event_handling/MotionEvent.java
@@ -1,8 +1,11 @@
 package a.event_handling;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MotionEvent extends AppCompatActivity {
 
@@ -10,5 +13,63 @@ public class MotionEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motion_event);
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, android.view.MotionEvent m) {
+                handleTouch(m);
+                return true;
+            }
+
+        });
+
     }
+
+    private void handleTouch(android.view.MotionEvent m) {
+        TextView textView1 = findViewById(R.id.textView2);
+        TextView textView2 = findViewById(R.id.textView3);
+
+        int pointerCount = m.getPointerCount();
+
+        for (int i = 0; i < pointerCount; i++)
+        {
+            int x = (int) m.getX(i);
+            int y = (int) m.getY(i);
+            int id = m.getPointerId(i);
+            int action = m.getActionMasked();
+            int actionIndex = m.getActionIndex();
+            String actionString;
+
+
+            switch (action)
+            {
+                case android.view.MotionEvent.ACTION_DOWN:
+                    actionString = "DOWN";
+                    break;
+                case android.view.MotionEvent.ACTION_UP:
+                    actionString = "UP";
+                    break;
+                case android.view.MotionEvent.ACTION_POINTER_DOWN:
+                    actionString = "PNTR DOWN";
+                    break;
+                case android.view.MotionEvent.ACTION_POINTER_UP:
+                    actionString = "PNTR UP";
+                    break;
+                case android.view.MotionEvent.ACTION_MOVE:
+                    actionString = "MOVE";
+                    break;
+                default:
+                    actionString = "";
+            }
+
+            String touchStatus = "Action: " + actionString + " Index: " +
+                    actionIndex + " ID: " + id + " X: " + x + " Y: " + y;
+            if (id == 0)
+                textView1.setText(touchStatus);
+            else
+                textView2.setText(touchStatus);
+        }
+
+    }
+
 }
