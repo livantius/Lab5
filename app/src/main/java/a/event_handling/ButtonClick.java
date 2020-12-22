bae50c16e90cecfbd86c24e17cb46e24e6c9481e
livantius <76242373+livantius@users.noreply.github.com>
2020-12-22 15:34:48 +0700

    Создание меню с опциями: ButtonClick, MotionEvent и CommonGestures.

---
+++ app/src/main/java/a/event_handling/ButtonClick.java
@@ -0,0 +1,14 @@
+package a.event_handling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonClick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_click);
+    }
+}
